0793317b24f7c696b71c9024378fd16596172c92
sivakumar <gsivadevops@gmail.com>
2025-10-04 05:28:55 +0530

    zoneid and name updated

--- 80-eks/variables.tf
+++ 80-eks/variables.tf
@@ -7,9 +7,9 @@ variable "environment" {
 }
 
 variable "zone_id" {
-    default = "Z032558618100M4EJX8X4"
+    default = "Z02979802YJHT5DLYSTHT"
 }
 
 variable "zone_name" {
-    default = "daws84s.site"
+    default = "devopslearning.store"
 }
