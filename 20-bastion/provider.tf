0578c8eda7dbe491b4a0311345b6a96f6fb7cf8b
sivakumar <gsivadevops@gmail.com>
2025-10-04 09:09:58 +0530

    bucket name updated

--- 20-bastion/provider.tf
+++ 20-bastion/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "eks-84s-remote-state-dev"
+    bucket = "tf-eks-remote-state-dev"
     key    = "roboshop-dev-bastion"
     region = "us-east-1"
     encrypt        = true
